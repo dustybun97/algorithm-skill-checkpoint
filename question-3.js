f6a8bedace169346975b4d76dafbcc1d09c8064b
podusit <d.rimdusit@gmail.com>
2024-12-24 10:49:46 +0700

    feat:algorithm skill checkpoint

--- question-3.js
+++ question-3.js
@@ -1,20 +1,19 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: Big O notation for Function findStudentById is O(n) because the function has a loop that iterates through the array member. The time complexity of the function is directly proportional to the number of members in the array.
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: Big O notation for Function findProductPrice is O(log n). The time complexity of the function is logarithmic because the search space is reduced by half in each iteration. As the number of products increases, the time complexity of the function grows logarithmically.
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
-
+Answer: Function findProductPrice is more efficient than Function findStudentById because the time complexity of findProductPrice is O(log n), which is better than O(n) for findStudentById. The binary search algorithm used in findProductPrice has a logarithmic time complexity, which is more efficient than the linear time complexity of the loop in findStudentById. As the number of elements increases, the binary search algorithm will perform better than the linear search algorithm in terms of time complexity.
 
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,4 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
-
-
+Answer: Big O notation for Function sortProductsByPrice is O(n^2) because the function has two nested loops that iterate through the array members. The time complexity of the function is directly proportional to the square of the number of members in the array.
 */
